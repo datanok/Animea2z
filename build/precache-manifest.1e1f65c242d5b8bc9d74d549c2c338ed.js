3d4d28c2ebbf59d7ced75f09c58e8f3f5ecc2ee4
tanmay <tanmaypatiltp25@gmail.com>
2023-03-03 14:01:04 +0530

    deplying

---
+++ build/precache-manifest.1e1f65c242d5b8bc9d74d549c2c338ed.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "978420cee7852edff273",
+    "url": "/animeapp/static/css/main.f4ab77f5.chunk.css"
+  },
+  {
+    "revision": "978420cee7852edff273",
+    "url": "/animeapp/static/js/main.978420ce.chunk.js"
+  },
+  {
+    "revision": "afc9b727bbe2ec6cff23",
+    "url": "/animeapp/static/js/1.afc9b727.chunk.js"
+  },
+  {
+    "revision": "a8d86329e9afbb054f04",
+    "url": "/animeapp/static/css/2.d8c66875.chunk.css"
+  },
+  {
+    "revision": "a8d86329e9afbb054f04",
+    "url": "/animeapp/static/js/2.a8d86329.chunk.js"
+  },
+  {
+    "revision": "1fd01c795848aa25db6b",
+    "url": "/animeapp/static/js/runtime~main.1fd01c79.js"
+  },
+  {
+    "revision": "592643a83b8541edc52063d84c468700",
+    "url": "/animeapp/static/media/fa-brands-400.592643a8.eot"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/animeapp/static/media/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/animeapp/static/media/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "b0e2db3b634d1bc3928e127458d993d8",
+    "url": "/animeapp/static/media/fa-regular-400.b0e2db3b.eot"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/animeapp/static/media/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "513aa607d398efaccc559916c3431403",
+    "url": "/animeapp/static/media/fa-brands-400.513aa607.ttf"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/animeapp/static/media/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/animeapp/static/media/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "c5d109be8edd3de0f60eb472bd9ef691",
+    "url": "/animeapp/static/media/fa-regular-400.c5d109be.svg"
+  },
+  {
+    "revision": "766913e6c0088ab8c9f73e18b4127bc4",
+    "url": "/animeapp/static/media/fa-regular-400.766913e6.ttf"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/animeapp/static/media/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "0c6bfc668a72935760178f91327aed3a",
+    "url": "/animeapp/static/media/fa-solid-900.0c6bfc66.eot"
+  },
+  {
+    "revision": "b9625119ce4300f0ef890a8f3234c773",
+    "url": "/animeapp/static/media/fa-solid-900.b9625119.ttf"
+  },
+  {
+    "revision": "1d5619cd804367cefe6da2d79289218a",
+    "url": "/animeapp/static/media/fa-brands-400.1d5619cd.svg"
+  },
+  {
+    "revision": "37bc7099f6f1ba80236164f22e905837",
+    "url": "/animeapp/static/media/fa-solid-900.37bc7099.svg"
+  },
+  {
+    "revision": "95eb551166b44f88118b966f1e343fbd",
+    "url": "/animeapp/index.html"
+  }
+];
